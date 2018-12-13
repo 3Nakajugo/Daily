3956ec05e7dee8aebfc2a46de96166730bcd9bc8
edna <edna@rr.com>
2018-12-13 17:12:22 +0300

    add square.py, separate.py

---
+++ separate.py
@@ -0,0 +1,10 @@
+def separate():
+    items = []
+    for item in range(2000, 3600):
+        if item % 7 == 0 and item % 5 != 0:
+            items.append(item)
+
+    print(items)
+
+
+separate()
---
+++ square.py
@@ -0,0 +1,9 @@
+
+def square():
+    digits = []
+    for x in range(1, 21):
+        digits.append(x**2)
+    print(digits[0:5])
+
+
+square()
